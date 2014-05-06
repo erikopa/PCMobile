31dd55d18328128695c0abbc5c691764ad796f0b
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-06 15:39:10 -0300

    Use ActionBarActivity for all the activities

--- src/org/tasafo/mobile/palestrascoletivas/FormEventActivity.java
+++ src/org/tasafo/mobile/palestrascoletivas/FormEventActivity.java
@@ -2,12 +2,20 @@ package org.tasafo.mobile.palestrascoletivas;
 
 import android.app.*;
 import android.os.*;
+import android.support.v7.app.*;
+import android.view.*;
 
-public class FormEventActivity extends Activity {
+public class FormEventActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_form_event);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.form_menu, menu);
+		return true;
 	}
 }
